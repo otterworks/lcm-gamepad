9d7d8a45ffe04faafe31f651fc864b9d9f3b3368
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2021-11-02 07:51:46 -0700

    adjust button mapping for DualSense

--- src/dualsense.h
+++ src/dualsense.h
@@ -4,10 +4,10 @@
   "/dev/input/by-id/"                                                          \
   "usb-Sony_Interactive_Entertainment_Wireless_Controller-if03-event-joystick"
 
-#define EV_SURGE ABS_X          // L horizontal
-#define EV_SWAY ABS_Y           // L vertical
-#define EV_HEAVE ABS_Z          // R vertical
-#define EV_YAW ABS_RZ           // R horizontal
+#define EV_SURGE ABS_Y          // L vertical
+#define EV_SWAY ABS_X           // L horizontal
+#define EV_HEAVE ABS_RY         // R vertical
+#define EV_YAW ABS_RX           // R horizontal
 #define EV_AUTO_DEPTH BTN_X     // Triangle
 #define EV_AUTO_HEADING BTN_B   // X
 #define EV_START_TRACKING BTN_C // Circle
--- src/gamepad.h
+++ src/gamepad.h
@@ -1,15 +1,15 @@
-// #include "dualsense.h"
-#include "dualshock.h"
+#include "dualsense.h"
+// #include "dualshock.h"
 
 #define OUTPUT_PERIOD 100000000 // nanoseconds
 
 #define STICK_OUTPUT_CHANNEL "XJOY"
 #define BUTTON_OUTPUT_CHANNEL "XJOY_BUTTON"
 
-#define XJ_SURGE 1
-#define XJ_SWAY 2
-#define XJ_HEAVE 5
-#define XJ_YAW 0
+#define XJ_SURGE 0
+#define XJ_SWAY 1
+#define XJ_HEAVE 2
+#define XJ_YAW 5
 
 #define XJ_AUTO_DEPTH 0
 #define XJ_AUTO_HEADING 1
